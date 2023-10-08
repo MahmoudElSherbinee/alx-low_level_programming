fff571303a3681a069b25e8e52e57bd48b68b28f
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-07 18:14:31 -0700

    AutoPush for C task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - this is the master function
+ *
+ * Description: Putchar() enable us to print a character
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	/*putchar() print a character*/
+	int putchar(int a)
+{
+	int a;
+
+	while (a <= z)
+{
+	putchar("%c \n");
+	a = a + 1;
+}
+}
+	return (0);
+}
