846a1cd14ec1f9f0c57bbfa79a332375bcaf55e4
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-10 08:40:18 -0700

    AutoPush for functions task

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - function is the master function to check the lowercase char
+ *
+ * @c: check the input
+ *
+ * Return: Always 0 (success)
+*/
+
+void jack_bauer(void){
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++) {
+	for (minute = 0; minute < 60; minute++)
+{
+	printf("%02d:%02d\n", hour, minute);
+}
+}
+}
