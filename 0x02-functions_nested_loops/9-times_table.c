fe536a9fd21391228853f6ee55b48f8e6129b35c
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-10 11:50:45 -0700

    AutoPush for functions task

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * times_table - function is the master function to check the lowercase char
+ *
+ * Return: Always 0 (success)
+*/
+
+void times_table(void)
+{
+	int r;
+	int c;
+
+	for (r = 0; r <= 9; r++)
+{
+	for (c = 0; c <= 9; c++)
+{
+	int table = r * c;
+}
+	printf("%d, ", table);
+}
+}
