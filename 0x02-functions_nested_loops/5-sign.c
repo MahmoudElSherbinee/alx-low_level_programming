e0d41b877c5c6bcb17d27ed55ac506872870f17e
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-10 07:47:10 -0700

    AutoPush for functions task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - function is the master function to check the lowercase char
+ *
+ * @n: check the input
+ *
+ * Return: Always 0 (success)
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+{
+	_putchar('+');
+	return (1);
+}
+	else if (n < 0)
+{
+	_putchar('-');
+	return (-1);
+}
+	else if (n == 0)
+{
+	_putchar('0');
+	return (0);
+}
+}
