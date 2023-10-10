ff94c0ee4ff3059e41f1adef4d9dd63dd7596a7b
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-10 14:21:00 -0700

    AutoPush for functions task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,12 +12,34 @@
 
 void print_times_table(int n)
 {
-	if (n < 0 || n > 15)
+	int x;
+	int y;
+	int z;
+
+	if (n <= 15 && n >= 0)
 {
-	return ();
+		for (x = 0; x <= n; x++)
+{
+			_putchar(48);
+			for (y = 1; y <= n; y++)
+{
+				_putchar(',');
+				_putchar(' ');
+				z = x * y;
+				if (z <= 9)
+					_putchar(' ');
+				if (z <= 99)
+					_putchar(' ');
+				if (z >= 100)
+{
+					_putchar((z / 100) + 48);
+					_putchar((z / 10) % 10 + 48);
 }
-	for (int i = 0; i <= 10; i++)
-{
-		printf("%d * %d = %d\n", i, n, i * n);
+				else if (z <= 99 && z >= 10)
+					_putchar((z / 10) + 48);
+				_putchar((z % 10) + 48);
+}
+			_putchar('\n');
+}
 }
 }
