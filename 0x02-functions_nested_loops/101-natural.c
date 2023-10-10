0a41f7674175a6fb1fd845dea150f4e487dc9332
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-10 15:06:38 -0700

    AutoPush for functions task

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - function is the master function
+ *
+ * Description: computes and prints the sum of all the multiples
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int sum = 0;
+
+	for (int i = 3; i < 1024; i++)
+{
+	if (i % 3 == 0 || i % 5 == 0)
+{
+	sum += i;
+}
+}
+	printf("%d\n", sum);
+	return (0);
+}
