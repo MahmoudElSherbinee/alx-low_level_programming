0c85930f4538fe92173225b0ce36fc94d63afbc6
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-10 15:39:06 -0700

    AutoPush for functions task

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - this is the master function
+ *
+ * Description: enable us to write a standard output
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int n = 50;
+	unsigned long long fib1 = 1;
+	unsigned long long fib2 = 2;
+	unsigned long long next;
+
+	printf("%llu, %llu, ", fib1, fib2);
+	for (int i = 3; i <= n; i++)
+{
+	next = fib1 + fib2;
+
+	if (i == n)
+{
+	printf("%llu\n", next);
+}
+	else
+{
+	printf("%llu, ", next);
+}
+	fib1 = fib2;
+	fib2 = next;
+}
+	return (0);
+}
+
