439adc5f7c46a4db1ac724d7055dc31a9ce02892
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-10 17:16:10 -0700

    AutoPush for functions task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,27 @@
 #include <stdio.h>
+
+/**
+ * numLength - this is the master function
+ *
+ * @num: enable us to write a standard output
+ *
+ * Return: number of digist in num
+ */
+
+int numLength(int num)
+{
+	int lenght = 0;
+
+	if (!num)
+		return (1);
+	while (num)
+{
+		num = num / 10;
+		length = length + 1;
+}
+	return (length);
+}
+
 /**
  * main - this is the master function
  *
@@ -9,27 +32,39 @@
 
 int main(void)
 {
-	int n = 98;
-	unsigned long int fib1 = 1;
-	unsigned long int fib2 = 2;
-	int i = 3;
+	int count;
+	int initial0s;
+	unsigned long f1 = 1;
+	unsigned long f2 = 2;
+	unsigned long sum;
+	unsigned long max = 100000000;
+	unsigned long f1o = 0;
+	unsigned long f2o = 0;
+	unsigned long sumo = 0;
 
-	printf("%lu, %lu, ", fib1, fib2);
+	for (count = 1; count <= 98; count++)
+{
+	if (f1o > 0)
+		printf("%lu", f1o);
+	initial0s = numLength(max) - 1 - numLength(f1);
+	while (f1o > 0 && initial0s > 0)
+	{
+		printf("%d", 0);
+		initial0s--;
+	}
+	printf("%lu", f1);
 
-	while (i <= n)
-{
-	if (i % 2 == 1)
-{
-		fib1 = fib1 + fib2;
-		printf("%lu, ", fib1);
-}
+	sum = (f1 + f2) % max;
+	sumo = f1o + f2o + (f1 + f2) / max;
+	f1 = f2;
+	f1o = f2o;
+	f2 = sum;
+	f2o = sumo;
+
+	if (count != 98)
+		printf(", ");
 	else
-{
-		fib2 = fib1 + fib2;
-		printf("%lu, ", fib2);
+		printf("\n");
 }
-	i++;
-}
-	printf("\n");
 	return (0);
 }
