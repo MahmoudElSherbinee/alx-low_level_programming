f284ecfef9f8e1df9bac28dae2ec3de6b579073a
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-12 02:40:54 -0700

    AutoPush for the More functions and nested loops task

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ *
+ * @n: check the inputs
+ *
+ * Return: 0 if the input is upper and 1 if it's not
+*/
+
+void print_line(int n)
+{
+	for (int i = 0; i < n; i++)
+{
+	_putchar(95);
+}
+	_putchar('\n');
+}
