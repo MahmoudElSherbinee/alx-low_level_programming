c37cb9a782416e34897eab6580b87bb78cee329b
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-12 03:38:26 -0700

    AutoPush for the More functions and nested loops task

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal -  draws a diagonal line on the terminal
+ *
+ * @n: check the inputs
+ *
+ * Return: 0 if the input is upper and 1 if it's not
+*/
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+{
+	putchar('\n');
+}
+	else
+{
+	for (int i = 1; i <= n; i++)
+{
+	for (int j = 1; j < i; j++)
+{
+	putchar(' ');
+}
+	putchar(92);
+	putchar('\n');
+}
+}
+}
