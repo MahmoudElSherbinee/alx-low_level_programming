a6952e017fc3470fb4afa0d878cfc819167a9d4b
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-12 17:29:38 +0300

    AutoPush for the More functions and nested loops task

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - Prints an integer
+ * @n: The integer to print
+ */
+#include "main.h"
+
+void print_number(int n)
+{
+    if (n < 0)
+    {
+        _putchar(45);
+        n = -n;
+    }
+
+    if (n / 10 != 0)
+    {
+        print_number(n / 10);
+    }
+
+    _putchar('0' + n % 10);
+}
