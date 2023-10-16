4d1cdf9e828f6d4b2434c4220864a0c31a59cbe2
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-17 01:20:29 +0300

    autopush

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,9 +9,11 @@
 
 void print_array(int *a, int n)
 {
+	int i;
+
 	if (a != NULL && n > 0)
 	{
-		for (int i = 0; i < n; i++)
+		for (i = 0; i < n; i++)
 
 		{
 			printf("%d", a[i]);
@@ -23,3 +25,4 @@ void print_array(int *a, int n)
 		}
 		printf("\n");
 	}
+}
