8c5c3e7ca2869c7e26f019afa5b2b8f1375e6f55
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-16 23:30:49 +0300

    autopush

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,18 @@
+#include "main.h"
+
+/**
+* _puts - writes the character c to stdout
+* @str: The character to print
+*
+* Return: On success 1.
+*/
+
 void _puts(char *str)
 {
-    
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
 }
