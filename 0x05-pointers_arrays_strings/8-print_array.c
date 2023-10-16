c1da6c7ef4aa0f93ca41011beb653c50e872e043
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-17 01:19:45 +0300

    autopush

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,19 +2,16 @@
 
 /**
 * print_array - writes the character c to stdout
-* @a: check the input
-*
-* @n: check the input
+* @a: The character to print
+* @n: The character to print
 * Return: On success 1.
 */
 
 void print_array(int *a, int n)
 {
-	int i;
-
 	if (a != NULL && n > 0)
 	{
-		for (i = 0; i < n; i++)
+		for (int i = 0; i < n; i++)
 
 		{
 			printf("%d", a[i]);
@@ -24,7 +21,5 @@ void print_array(int *a, int n)
 				printf(", ");
 			}
 		}
-
 		printf("\n");
 	}
-}
