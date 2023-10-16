bcec8abd7f339c041cbc5c48384116ed166b6520
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-16 23:01:17 +0300

    autopush

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _strlen - is a function that takes a pointer to an int as parameter
+* and updates the value it points to to 98
+*
+* @s: check the input
+*
+* Return: 0 always success
+*/
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (i < s[i])
+	{
+		i++;
+	}
+	return (i);
+}
