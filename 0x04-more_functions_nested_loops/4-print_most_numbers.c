c5bef8b838206616b83f7e9a9250ac9188a95d96
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-16 22:04:27 +0300

    first

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,22 +7,22 @@
  * Description: this function prints most numbers
  *
  * Return: 0 if the input is upper and 1 if it's not
-*/
+ */
 
 void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9 ; i++)
-{
-	if (i == 2 || i == 4)
-{
-	continue;
-}
-	else
-{
-	_putchar(i + '0');
-}
-}
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i + '0');
+		}
+	}
 	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,7 +7,7 @@
  * @n: check the inputs
  *
  * Return: 0 if the input is upper and 1 if it's not
-*/
+ */
 
 void print_diagonal(int n)
 {
@@ -15,19 +15,19 @@ void print_diagonal(int n)
 	int j;
 
 	if (n <= 0)
-{
-	putchar('\n');  
-}
+	{
+		putchar('\n');
+	}
 	else
-{
-	for (i = 1; i <= n; i++)
-{
-	for (j = 1; j < i; j++)
-{
-	putchar(' ');
-}
-	putchar(92);
-	putchar('\n');
-}
-}
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j < i; j++)
+			{
+				putchar(' ');
+			}
+			putchar(92);
+			putchar('\n');
+		}
+	}
 }
