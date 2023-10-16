e79f76924b720d308515937eb941ea8f59c84bb7
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-17 01:38:43 +0300

    autopush

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _strcpy- writes the character c to stdout
+*
+* @dest: check the input
+* @src: check the input
+*
+* Return: On success 1.
+*/
+
+char *_strcpy(char *dest, char *src)
+
+{
+	char *original_dest = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
+	*dest = '\0';
+}
