6db05f5aad6d75e609ce69d694b60c5dff240008
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-19 18:45:25 +0300

    fourth 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,41 +1,76 @@
 #include "main.h"
 
 /**
-* cap_string - the master function
-*
-* @n:check the input2
-*
-* Return: the char
-*/
+ * lower - function to check if the character is lower case
+ *
+ * @c: input to check the character
+ *
+ * Return: the character if it is lower case
+ */
 
-char *cap_string(char *n)
+int lower(char c)
 {
-	int i;
+	return (c >= 97 && c <= 122);
+}
 
+/**
+ * separator - fuction to list the separators and check the index
+ *
+ * @c: check the character
+ *
+ * Return: 1 if the character is separator and 0 if it is not
+ */
+
+int separator(char c)
+{
 	int x;
-
-	int j;
-
 	char separators[] = " \t\n,;.!?\"(){}";
 
 	for (x = 0; x < 13; x++)
 	{
-		for (i = 0; n[i] != '\0'; i++)
+		if (c == separators[x])
 		{
-			if (n[i] == separators[x])
-			{
-				j = i + 1;
-				while (n[j] != '\0' && !(n[j] >= 'A' && n[j] <= 'Z') && !(n[j] >= 'a' && n[j] <= 'z'))
-				{
-					j++;
-				}
-				if (n[j] >= 'a' && n[j] <= 'z')
-				{
-					n[j] -= 32;
-				}
-				i = j;
-			}
+			return (1);
+		}
+		else
+		{
+			return (0);
 		}
 	}
-	return (n);
+}
+
+/**
+ * cap_string - the master function
+ *
+ * @n:check the input2
+ *
+ * Return: the char
+ */
+
+char *cap_string(char *n)
+{
+	int i;
+	char *p;
+
+	i = 0;
+	p = n;
+
+	while (*n)
+	{
+		if (separator(*n))
+		{
+			i = 1;
+		}
+		else if (lower(*n) && i)
+		{
+			*n = *n - 32;
+			i = 0;
+		}
+		else
+		{
+			i = 0;
+		}
+		n++;
+	}
+	return (p);
 }
