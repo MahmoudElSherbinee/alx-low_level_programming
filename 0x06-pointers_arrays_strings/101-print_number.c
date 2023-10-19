2cc1c44d567313c018d56762249e38b9450b0347
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-19 21:54:44 +0300

    second easy 101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,16 +10,19 @@
 
 void print_number(int n)
 {
+	unsigned int number;
+
+	number = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		number = -n;
 	}
 
-	if (n / 10 != 0)
+	if (number / 10 != 0)
 	{
-		print_number(n / 10);
+		print_number(number / 10);
 	}
 
-	_putchar(n % 10 + '0');
+	_putchar('0' + (number % 10));
 }
