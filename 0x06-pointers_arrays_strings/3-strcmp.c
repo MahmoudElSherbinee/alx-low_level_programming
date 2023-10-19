3aab74af4ad5412336218eb7c8e0d37d9638391b
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-19 05:30:05 +0300

    fourth try

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,34 +14,15 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	int length1;
+	i = 0;
 
-	int length2;
-
-	length1 = 0;
-	length2 = 0;
-
-	for (i = 0; s1[i] != '\0'; i++)
-
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		length1++;
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
 	}
-	for (i = 0; s2[i] != '\0'; i++)
-
-	{
-		length2++;
-	}
-
-	if (length1 > length2)
-	{
-		return (*s1 - *s2);
-	}
-	if (length1 < length2)
-	{
-		return (*s1 - *s2);
-	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
