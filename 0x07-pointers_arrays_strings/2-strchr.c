1f588b4dcdc60fdb273359c5d8dcc8c0dfa3e386
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-23 22:17:31 +0300

    I don't know 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,14 +11,9 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	while (*s != c && *s != '\0')
 	{
-		if (*s <= c)
-		{
-			return (s);
-		}
 		s++;
 	}
-
-	return (NULL);
+	return ((*s == c) ? s : NULL);
 }
