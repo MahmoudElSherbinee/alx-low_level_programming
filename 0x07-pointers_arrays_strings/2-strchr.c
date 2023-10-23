24709378b65257d69ef2e07be049554eabb63731
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-23 22:11:39 +0300

    first try 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _strchr - prints buffer
+*
+* @s: check the input1
+* @c: check the input2
+*
+* Return: Nothing.
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+
+	return (NULL);
+}
