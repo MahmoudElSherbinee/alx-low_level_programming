a41a4b73210309488d3a2bd638ce3883d7b81357
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-23 22:22:58 +0300

    firstt try 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+* _strspn - prints buffer
+*
+* @s: check the input1
+* @accept: check the input2
+*
+* Return: Nothing.
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int length = 0;
+
+	int match;
+
+	while (*s)
+	{
+		match = 0;
+		char *a = accept;
+
+		while (*a)
+		{
+			if (*s == *a)
+			{
+				match = 1;
+				break;
+			}
+			a++;
+		}
+
+		if (match)
+		{
+			length++;
+			s++;
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	return (length);
+}
