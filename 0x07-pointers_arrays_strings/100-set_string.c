37696f15513762701dc16bdcc2d27b0e1e5bb33e
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-23 23:15:01 +0300

    first try 100

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* set_string - prints buffer
+*
+* @s: check the input1
+* @to: check the input2
+*
+* Return: Nothing.
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
