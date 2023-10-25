ff8b61698983f0e55b69338c4fc675a5e0c9cc8e
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-26 00:48:24 +0300

    second 101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -18,10 +18,14 @@ int wildcmp_helper(char *s1, char *s2)
 
 	if (*s2 == '*')
 	{
-		if (wildcmp_helper(s1, s2 + 1) || (*s1 != '\0' && wildcmp_helper(s1 + 1, s2)))
+		if (wildcmp_helper(s1, s2 + 1))
 		{
 			return (1);
 		}
+	}
+	else if ((*s1 != '\0' && wildcmp_helper(s1 + 1, s2)))
+	{
+		return (1);
 	}
 
 	if (*s1 == *s2 || *s2 == '?')
