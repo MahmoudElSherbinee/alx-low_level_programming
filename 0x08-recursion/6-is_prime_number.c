f1f89fcf2e8e3dd2a69da16a7f3f1ac6afa796f2
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-26 00:30:07 +0300

    first 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+* is_prime_helper - master function
+*
+* @n: check the input
+* @divisor: check the input
+*
+* Return: the required pow
+*/
+
+int is_prime_helper(int n, int divisor)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	if (divisor == 1)
+	{
+		return (1);
+	}
+	if (n % divisor == 0)
+	{
+		return (0);
+	}
+	return (is_prime_helper(n, divisor - 1));
+}
+
+/**
+* is_prime_number - master function
+*
+* @n: check the input
+*
+* Return: the required pow
+*/
+
+int is_prime_number(int n)
+{
+	return (is_prime_helper(n, n / 2));
+}
