1910b17e8205f68e98728f74d8e6ac053e4d1f1b
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-27 16:20:50 +0200

    first 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - the master function
+*
+* @argc: check the number of inputs
+* @argv: check the value of the input
+*
+* Return: 0 always success
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
