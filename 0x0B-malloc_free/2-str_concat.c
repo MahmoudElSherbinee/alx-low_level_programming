f9480596e2e85c7451da086d09ce734f50ca6f6b
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-30 21:03:20 +0200

    second 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,6 +24,14 @@ char *str_concat(char *s1, char *s2)
 	len1 = 0;
 	len2 = 0;
 
+    if (s1 == NULL)
+    {
+		s1 = "";
+    }
+    if (s2 == NULL)
+    {
+		s2 = "";
+    }
 	for (i = 0; s1[i]; i++)
 	{
 		len1++;
