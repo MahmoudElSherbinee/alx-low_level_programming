6bcbb117766a1aca1ee831995c8075f49746bc0f
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-30 21:01:33 +0200

    first 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+* str_concat - function for creating array
+*
+* @s1: check the first string
+* @s2: check the second string
+*
+* Return: 0 Always success
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	char *ptr;
+
+	unsigned int len1;
+
+	unsigned int len2;
+
+	int i;
+
+	int size;
+
+	len1 = 0;
+	len2 = 0;
+
+	for (i = 0; s1[i]; i++)
+	{
+		len1++;
+	}
+	for (i = 0; s2[i]; i++)
+	{
+		len2++;
+	}
+	size = len1 + len2;
+	ptr = malloc(sizeof(char) * size + 1);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		ptr[i] = s1[i];
+	}
+	for (i = 0; s2[i] != '\0'; i++)
+	{
+		ptr[len1 + i] = s2[i];
+	}
+	ptr[size] = '\0';
+
+	return (ptr);
+}
