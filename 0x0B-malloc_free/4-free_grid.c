f359ab4179de4ae49d02cb25a42628fa5aaf9820
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-31 16:47:34 +0200

    first 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* free_grid - function for creating array
+*
+* @grid: check required width of the array
+* @height: check required height of the array
+*
+* Return: 0 Always success
+*/
+
+void free_grid(int **grid, int height)
+{
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+
+	for (int i = 0; i < height; i++)
+
+	{
+		if (grid[i] != NULL)
+		{
+			free(grid[i]);
+		}
+	}
+
+	free(grid);
+}
