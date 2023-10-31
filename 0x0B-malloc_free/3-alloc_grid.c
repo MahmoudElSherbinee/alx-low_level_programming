dc8b09e831196ef95664f027c4abb4739b2a45d4
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-10-31 16:38:53 +0200

    first 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+* alloc_grid - function for creating array
+*
+* @width: check required width of the array
+* @height: check required height of the array
+*
+* Return: 0 Always success
+*/
+int **alloc_grid(int width, int height)
+{
+	int i;
+
+	int j;
+
+	int **grid;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	grid = (int **)malloc(height * sizeof(int *));
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			return (NULL);
+		}
+	}
+
+	return (grid);
+}
