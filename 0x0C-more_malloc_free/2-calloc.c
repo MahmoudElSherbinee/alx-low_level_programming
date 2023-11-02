cccc103042ff5c020787e050ebc5dcecc728c480
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-11-02 23:13:43 +0200

    first 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* _calloc - fuction that making memory allocation
+*
+* @nmemb: check the number of bytes to be located
+* @size: check the number of bytes to be located
+*
+* Return: address
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *ptr;
+
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(sizeof(size) * nmemb);
+	if (ptr == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < nmemb; i++)
+	{
+		ptr[i] = 0;
+	}
+	return (ptr);
+}
