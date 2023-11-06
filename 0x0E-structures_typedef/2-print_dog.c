609fd9c6f7d2705cba40bf461af89706fc3ccf31
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-11-07 00:30:44 +0200

    first 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+
+/**
+ * print_dog - function that initialize a variable of type struct dog
+ *
+ * @d: the struct of the data type
+ *
+ * Return: Nothing
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	else
+	{
+		printf("Name: %s\n", ((d->name != NULL) ? d->name : "(nil)"));
+		printf("Age: %s\n", d->age);
+		printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
 /**
 * struct dog - Short description
 * @name: First member
@@ -22,6 +23,6 @@ struct dog
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
+void print_dog(struct dog *d);
 
 #endif
