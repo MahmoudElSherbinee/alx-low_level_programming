857becb9900a0c32156be856b4a291715da91aee
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-11-08 20:53:55 +0200

    first 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+* array_iterator - is a function that prints a name
+*
+* @array: check the array want to work with it
+* @size: is the size of the array
+* @action: is a pointer to the function you need to use
+*
+* Return: Nothing
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
