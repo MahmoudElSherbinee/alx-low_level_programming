7a203d7238f4095f0886207e7a2a8805e49f0f6b
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-11-08 21:04:21 +0200

    first 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include "function_pointers.h"
+
+/**
+* int_index - is a function that prints a name
+*
+* @array: check the array want to work with it
+* @size: is the number of elements of the array
+* @cmp: is a pointer to the function you need to use
+*
+* Return: Nothing
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+
+	return (-1);
+}
