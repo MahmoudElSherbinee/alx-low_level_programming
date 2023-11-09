d973908b2bc757255e10dbfccbdc15f1e9e1946a
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-11-09 23:05:07 +0200

    second 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,54 +1,57 @@
 #include "variadic_functions.h"
 
 /**
-* print_all - function that prints strings, followed by a new line.
-*
-* @format: the number of data type to be printed
-* @...: the arguments will be passed
-*
-* Return: Nothing.
-*/
+ * print_all - function that prints strings, followed by a new line.
+ *
+ * @format: the number of data type to be printed
+ * @...: the arguments will be passed
+ *
+ * Return: Nothing.
+ */
 
 void print_all(const char *const format, ...)
 {
-	int x, i;
-	char c;
-	float f;
-	char *s;
-	va_list arguments;
-	int numofelem = strlen(format);
+    va_list arguments;
+    int numofelem = strlen(format);
+    int x;
+    va_start(arguments, format);
 
-	va_start(arguments, format);
-	for (x = 0; x < numofelem; x++)
-	{
-		if (format[x] == 'c')
-		{
-			c = va_arg(arguments, int);
-			printf("%c", c);
-		}
-		else if (format[x] == 'i')
-		{
-			i = va_arg(arguments, int);
-			printf("%d", i);
-		}
-		else if (format[x] == 'f')
-		{
-			f = va_arg(arguments, double);
-			printf("%f", f);
-		}
-		else if (format[x] == 's')
-		{
-			s = va_arg(arguments, char *);
-			if (s == NULL)
-				printf("(nil)");
-			else
-				printf("%s", s);
-		}
-		else if (format[x] != 'c' || format[x] != 'i'
-		|| format[x] != 'f' || format[x] != 's')
-			continue;
-		if (x != numofelem - 1)
-			printf(", ");
-	}
-	printf("\n");
+    for (x = 0; x < numofelem; x++)
+    {
+        if (format[x] == 'c')
+        {
+            char c = va_arg(arguments, int);
+            printf("%c", c);
+        }
+        else if (format[x] == 'i')
+        {
+            int i = va_arg(arguments, int);
+            printf("%d", i);
+        }
+        else if (format[x] == 'f')
+        {
+            double f = va_arg(arguments, double);
+            printf("%f", f);
+        }
+        else if (format[x] == 's')
+        {
+            char *s = va_arg(arguments, char *);
+            if (s == NULL)
+                printf("(nil)");
+            else
+                printf("%s", s);
+        }
+        else
+        {
+            continue;
+        }
+
+        if (x != numofelem - 1)
+        {
+            printf(", ");
+        }
+    }
+
+    va_end(arguments);
+    printf("\n");
 }
