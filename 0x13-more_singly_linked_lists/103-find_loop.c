c33e959760b6d351805a92e79d3baed5d5b262a4
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-11-22 01:23:35 +0200

    second 103

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,5 +1,14 @@
 #include "lists.h"
 
+/**
+ * find_listint_loop - Finds the loop in a singly linked list.
+ *
+ * @head: A pointer to the head of the singly linked list.
+ *
+ * Return: A pointer to the node where the loop starts, or NULL if there is
+ * no loop.
+ */
+
 listint_t *find_listint_loop(listint_t *head)
 {
 	/* Initialize tortoise and hare pointers */
