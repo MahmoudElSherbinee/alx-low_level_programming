700c9eb6a64f79bd61b0ee2ea48931bf8a0c9f7a
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-11-21 23:36:57 +0200

    third 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -20,7 +20,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		temp = temp->next;
 	}
 
-	if (head == NULL || i < index)
+	if (temp == NULL || i < index)
 	{
 		return (NULL);
 	}
