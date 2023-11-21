01f8c0f762a4d3f3159c78dde2f2d4e3d594a1d3
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-11-22 00:59:30 +0200

    second 100

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -12,17 +12,16 @@
 */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *current = *head;
 	listint_t *previous = NULL;
 	listint_t *next;
 
-	while (current)
+	while (*head)
 	{
-		next = current->next;
+		next = (*head)->next;
 
-		current->next = previous;
-		previous = current;
-		current = next;
+		(*head)->next = previous;
+		previous = *head;
+		*head = next;
 	}
 
 	*head = previous;
