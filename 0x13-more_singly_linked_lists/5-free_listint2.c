1bdfd10bdb9acd907f1ca732e75d2c472e703cd3
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-11-21 20:57:16 +0200

    third 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,12 +13,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current, *last;
 
-	current = *head;
-
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 	{
 		return;
 	}
+
+	current = *head;
 
 	while (current != NULL)
 	{
