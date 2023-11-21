b864fce71ed6e7e587ec96d45b771fc3ae9e264e
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-11-21 20:53:38 +0200

    first 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+* free_listint2 - Frees a list_t list.
+*
+* This function frees the memory allocated for each node in the linked list,
+* including the integers within each node.
+*
+* @head: A pointer to the head of the linked list.
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current, *last;
+
+	current = *head;
+
+	while (current != NULL)
+	{
+		last = current->next;
+		free(current);
+		current = last;
+	}
+	*head = NULL;
+}
