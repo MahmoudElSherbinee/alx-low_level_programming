5282f3b8b3c09c3cc741cfd7d5850ab8c1855878
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-11-27 23:38:08 +0200

    no * 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,68 +1,56 @@
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <stdio.h>
-#include <stdlib.h>
+/**
+* main - Copies the content from one file to another.
+*
+* @ac: Count of command-line arguments
+* @av: Array of command-line arguments
+*
+* Return: 0 on success
+*/
+int main(int ac, char **av)
+{
+	int src, dst, bytes_read, bytes_written;
 
+	char buffer[BUFFSIZE];
 
+	if (ac != 3)
+		print_exit(97, "Usage: cp file_from file_to\n", av[0]);
 
-#define USE "Usage: cp file_from file_to\n"
-#define N_READ "Error: Can't read from file %s\n"
-#define N_WRITE "Error: Can't write to %s\n"
-#define N_CLOSE "Error: Can't close fd %d\n"
+	src = open(av[1], O_RDONLY);
+	if (src == -1)
+		print_exit(98, "Error: Can't read from file %s\n", av[1]);
 
+	dst = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (dst == -1)
+		print_exit(99, "Error: Can't write to %s\n", av[2]);
 
-#define BUFFER 1024
-#define PERMS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+	while ((bytes_read = read(src, buffer, BUFFSIZE)) > 0)
+	{
+		bytes_written = write(dst, buffer, bytes_read);
+		if (bytes_written == -1)
+			print_exit(99, "Error: Can't write to %s\n", av[2]);
+	}
+
+	if (bytes_read == -1)
+		print_exit(98, "Error: Can't read from file %s\n", av[1]);
+
+	if (close(src) == -1 || close(dst) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close file descriptor\n");
+		exit(100);
+	}
+
+	return (0);
+}
 
 /**
- * main - check the code
- *
- * @argc: skfmskfsfs
- * @argv: sofjslfjslf
- * Return: Always 0.
- */
-
-int main(int argc, char *argv[])
+* print_exit - Print an error message and exit with a code.
+*
+* @code: Exit code for the program
+* @msg: Error message to print
+* @file: File name associated with the error
+*/
+void print_exit(int code, const char *msg, const char *file)
 {
-	int  fd_from = 0, fd_to = 0;
-	ssize_t bytes;
-	char buffer[BUFFER];
-
-	if (argc != 3)
-	{dprintf(STDERR_FILENO, USE);
-		exit(97);
-	}
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{dprintf(STDERR_FILENO, N_READ, argv[1]);
-		exit(98);
-	}
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERMS);
-	if (fd_to == -1)
-	{dprintf(STDERR_FILENO, N_WRITE, argv[2]);
-		exit(99);
-	}
-	while ((bytes = read(fd_from, buffer, BUFFER)) > 0)
-	{
-		if (write(fd_to, buffer, bytes) != bytes)
-		{dprintf(STDERR_FILENO, N_WRITE, argv[2]);
-			exit(99);
-		}
-		if (bytes == -1)
-		{dprintf(STDERR_FILENO, N_READ, argv[1]);
-			exit(98);
-		}
-	}
-	fd_from = close(fd_from);
-	fd_to = close(fd_to);
-	if (fd_from)
-	{dprintf(STDERR_FILENO, N_CLOSE, fd_from);
-		exit(100);
-	}
-	if (fd_to)
-	{	dprintf(STDERR_FILENO, N_CLOSE, fd_from);
-		exit(100);
-	}
-	return (EXIT_SUCCESS);
+	dprintf(STDERR_FILENO, msg, file);
+	exit(code);
 }
