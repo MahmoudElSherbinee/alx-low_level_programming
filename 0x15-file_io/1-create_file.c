4e4883a740056b4203f28378b0593d512d882de2
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-11-27 17:54:09 +0200

    second 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -36,12 +36,5 @@ int create_file(const char *filename, char *text_content)
 	}
 	close(descriptor);
 
-	if (num_of_bytes == length)
-	{
-		return (1);
-	}
-	else
-	{
-		return (-1);
-	}
+	return (num_of_bytes == length ? 1 : -1);
 }
