2870441448cadb236c99f26dff9fc33de913c183
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-11-27 23:22:49 +0200

    17*3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 		if (bytes == -1)
-		{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
 			exit(98);
 		}
 	}
