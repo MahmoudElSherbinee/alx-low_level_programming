489b64a2f26ac5827186c8cea05eb3522e8132e2
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-11-27 19:18:25 +0200

    first 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,18 @@
 #include "main.h"
 
+/**
+ * append_text_to_file - Appends text content to an existing file or creates
+ *                       a new file if it does not exist.
+ *
+ * @filename: The name of the file to which text content will be appended.
+ * @text_content: The text content to be appended to the file.
+ *
+ * Return:
+ * On success, returns 1.
+ * On failure or if @filename is NULL, returns -1.
+ */
+
+
 int append_text_to_file(const char *filename, char *text_content)
 {
 	ssize_t descriptor;
