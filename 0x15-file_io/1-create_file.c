b7458773941f2eb62f5f6e80449774fb4fe6b83e
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-11-27 17:51:48 +0200

    second 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	ssize_t num_of_bytes = 0;
 	ssize_t length = strlen(text_content);
 
-	if (filename == NULL)
+	if (!filename)
 	{
 		return (-1);
 	}
