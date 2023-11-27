00088bbc0b12bd5f063773aa214b7d4af6e2a55a
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-11-27 17:51:10 +0200

    second 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,17 @@
 #include "main.h"
 
+/**
+ * create_file - Creates a new file with the specified filename
+ *               and writes the given text content to it.
+ *
+ * @filename: The name of the file to be created or overwritten.
+ * @text_content: The text content to be written to the file.
+ *
+ * Return:
+ * On success, returns 1.
+ * On failure or if @filename is NULL, returns -1.
+ */
+
 int create_file(const char *filename, char *text_content)
 {
 	int descriptor;
