973f8c1264aab3dcf5eabc1ab31bb4b5a034bcb2
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-11-27 16:09:43 +0200

    fourth 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,7 +18,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t num_of_bytes;
 	int discriptor;
-	char buffer[BUFFER];
+	char buffer[BUFFER * 8];
 
 	if (filename == NULL || !letters)
 	{
