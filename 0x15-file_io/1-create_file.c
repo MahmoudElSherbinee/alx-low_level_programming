38808aea70af36712bda98576e8a26944bf637ad
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2023-11-27 18:10:40 +0200

    fifth 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (descriptor == -1)
 	{
